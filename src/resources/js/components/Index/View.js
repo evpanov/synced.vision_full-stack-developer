162aee143d503e994f01588784c4c94644a36fd3
e.panov <e.panov@unlimint.com>
2022-02-24 13:36:31 +0200

    ReactJS app

--- src/resources/js/components/Index/View.js
+++ src/resources/js/components/Index/View.js
@@ -1,5 +1,5 @@
 import {Component} from "react";
-import {AlertTitle, Box, Container, ThemeProvider} from "@mui/material";
+import {AlertTitle, Box, Button, Container, Grid, TextField, ThemeProvider} from "@mui/material";
 import {muiTheme} from "../../muiTheme";
 
 export default class View extends Component {
@@ -8,7 +8,34 @@ export default class View extends Component {
             <ThemeProvider theme={muiTheme}>
                 <Box>
                     <Container maxWidth={"xl"}>
-                        <AlertTitle>Weather</AlertTitle>
+                        <h1>Weather</h1>
+                        <Grid container spacing={2}>
+                            <Grid item xs={6}>
+                                <TextField
+                                    id="outlined-basic"
+                                    label="City"
+                                    variant="standard"
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button
+                                    variant="contained"
+                                    color="secondary"
+                                    fullWidth
+                                    size={"large"}
+                                >Get from API</Button>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Button
+                                    variant="contained"
+                                    color="success"
+                                    fullWidth
+                                    size={"large"}
+                                >Get from DB</Button>
+                            </Grid>
+                        </Grid>
+                        <hr/>
                     </Container>
                 </Box>
             </ThemeProvider>
